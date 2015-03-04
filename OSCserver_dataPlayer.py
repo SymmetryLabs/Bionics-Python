e47855f91c1988f6ee4a31b15ba693df83ae6264
Trip Vest <trip.vest@gmail.com>
2015-03-03 19:08:25 -0800

    Data player successfully sending recorded OSC messages

---
+++ OSCserver_dataPlayer.py
@@ -0,0 +1,61 @@
+# Program to play motion data from data files
+# Sends to port 9050 (normally the Processing port)
+
+import json
+from datetime import datetime, timedelta
+
+from DataIOClasses import DataWriter, DataReader
+
+directory = 'tests/data/'
+filename = '20150303-183941.txt'
+datareader = DataReader( directory + filename )
+messageHistory = datareader.getEntries()
+# print messageHistory
+
+OSCport = 9050
+
+print ""
+print ""
+print ""
+print "--------------------"
+print "--------------------"
+print "OSC Data Player RUNNING"
+print "--------------------"
+print "--------------------"
+print ""
+
+print "Playing file: " + directory + filename
+print "Sending to OSC port: ", OSCport
+print messageHistory[-1]['time'].total_seconds(), " seconds of recorded data"
+print ""
+
+# From "client.py" in pyliblo examples
+
+import liblo, sys
+
+try:
+    target = liblo.Address(OSCport)
+except liblo.AddressError, err:
+    print str(err)
+    sys.exit()
+
+startTime = datetime.now()
+
+# Loop through all messages and send to Processing
+try:
+    for message in messageHistory:
+        msg = liblo.Message(message['path'])
+        for data in message['data']:
+            msg.add(data)
+        while ( (datetime.now()-startTime) < message['time'] ):
+            pass
+        liblo.send(target, msg)
+except KeyboardInterrupt:
+    pass
+
+
+print "--------------------"
+print "--------------------"
+print "OSC Data Player CLOSED"
+print "--------------------"
+print "--------------------"
